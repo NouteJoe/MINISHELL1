3ba68158c13a6c13ca49d97dce6a0846eb96ac57
Joanna Muni reddi <jmuni-re@k2r4p10.42mulhouse.fr>
2023-02-25 13:23:10 +0100

    norminette

--- structvar/ft_addback.c
+++ structvar/ft_addback.c
@@ -6,39 +6,44 @@
 /*   By: jmuni-re <jmuni-re@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:52:39 by jmuni-re          #+#    #+#             */
-/*   Updated: 2023/02/20 14:52:45 by jmuni-re         ###   ########.fr       */
+/*   Updated: 2023/02/25 13:19:47 by jmuni-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int ft_varsize(t_var *var) {
-  int len;
+int	ft_varsize(t_var *var)
+{
+	int	len;
 
-  len = 0;
-  if (var == NULL)
-    return (0);
-  while (var) {
-    var = var->next;
-    len++;
-  }
-  return (len);
+	len = 0;
+	if (var == NULL)
+		return (0);
+	while (var)
+	{
+		var = var->next;
+		len++;
+	}
+	return (len);
 }
 
-t_var *ft_varlast(t_var *var) {
-  int len;
+t_var	*ft_varlast(t_var *var)
+{
+	int	len;
 
-  len = ft_varsize(var);
-  while (len-- && len)
-    var = var->next;
-  return (var);
+	len = ft_varsize(var);
+	while (len-- && len)
+		var = var->next;
+	return (var);
 }
 
-void ft_varadd_back(t_var **var, t_var *n) {
-  if (var && n) {
-    if (*var)
-      ft_varlast(*var)->next = n;
-    else
-      *var = n;
-  }
+void	ft_varadd_back(t_var **var, t_var *n)
+{
+	if (var && n)
+	{
+		if (*var)
+			ft_varlast(*var)->next = n;
+		else
+			*var = n;
+	}
 }
--- structvar/ft_varnew.c
+++ structvar/ft_varnew.c
@@ -6,19 +6,20 @@
 /*   By: jmuni-re <jmuni-re@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:52:57 by jmuni-re          #+#    #+#             */
-/*   Updated: 2023/02/20 14:52:59 by jmuni-re         ###   ########.fr       */
+/*   Updated: 2023/02/25 13:22:33 by jmuni-re         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-t_var *ft_varnew() {
-  t_var *var;
+t_var	*ft_varnew(void)
+{
+	t_var	*var;
 
-  var = malloc(sizeof(t_var));
-  if (var == NULL)
-    return (NULL);
-  var->next = NULL;
-  init_struct(&var);
-  return (var);
+	var = malloc(sizeof(t_var));
+	if (var == NULL)
+		return (NULL);
+	var->next = NULL;
+	init_struct(&var);
+	return (var);
 }
